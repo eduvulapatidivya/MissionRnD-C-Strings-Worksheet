e87d9b6494301ed1f12b28f956b7eb0ff90b4cfd
eduvulapatidivya <eduvulapatidivya@missionrnd.com>
2016-03-15 19:50:24 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,7 +11,34 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
-
+void strreverse(char*input, int i, int j)
+{
+	char temp;
+	while (i < j)
+	{
+		temp = input[i];
+		input[i] = input[j];
+		input[j] = temp;
+		i++;
+		j--;
+	}
+}
 void str_words_in_rev(char *input, int len){
-	
+	int a = 0, i, j,count=0;
+	for (i = 0; i<len; i++)
+	{
+		if (input[i] == 32)
+		{
+			strreverse(input, a, i - 1);
+			a = i + 1;
+			count = 1;
+		}
+	}
+	if (count == 1)
+	{
+		strreverse(input, a, len - 1);
+
+		strreverse(input, 0, len - 1);
+	}
+
 }
--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -12,5 +12,24 @@ NOTES: Don't create new string.
 */
 
 char removeSpaces(char *str) {
+	int i,j=0;
+	if (str)
+	{
+		
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] == 32)
+			{ }
+			else
+			{
+				str[j] = str[i];
+				j++;
+			}
+		}
+		for (i = j; str[i] != '\0'; i++)
+		{
+			str[i] = '\0';
+		}
+	}
 	return '\0';
 }
